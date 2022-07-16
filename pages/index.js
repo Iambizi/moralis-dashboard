b146563ca5ebd0c9a9425852d19ac5331e99062e
Iambizi <amirbzmn@gmail.com>
2022-07-16 14:15:04 -0400

    Creates log in functionlality

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,49 @@
+import { Flex, Text, Button } from "@chakra-ui/react";
+import Head from "next/head";
 import { useMoralis } from "react-moralis";
-import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  const {} = useMoralis();
-  return (
-    <div className={styles.container}>
-      <h1>Wa gwan</h1>
-    </div>
-  );
-}
+const Home = () => {
+  const { isAuthenticated, authenticate } = useMoralis();
+  console.log(isAuthenticated);
+  if (!isAuthenticated) {
+    return (
+      <>
+        <Head>
+          <title>Login | Dashboard3</title>
+        </Head>
+        <Flex
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          width="100vw"
+          height="100vh"
+          bgGradient="linear(to-br,teal.400, purple.300)"
+        >
+          <Text fontSize="5xl" fontWeight="bold" color="white">
+            Dashboard3
+          </Text>
+          <Button
+            colorScheme="purple"
+            size="lg"
+            mt="6"
+            onClick={() => authenticate()}
+          >
+            Login with Metamask
+          </Button>
+        </Flex>
+      </>
+    );
+  }
+  return(
+    <>
+      <Head>
+        <title>Dashboard3</title>
+      </Head>
+      <Flex>
+        
+      </Flex>
+    </>
+  )
+};
+
+export default Home;
